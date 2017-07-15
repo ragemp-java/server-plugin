cc1aef9b93f37c649b1299aee6dc9a1590cf9bb4
Fabian Jungwirth <fabian@jungwirth.ovh>
2017-07-15 16:29:36 +0200

    Update rage sdk directly from george

--- src/sdk/Pools.hpp
+++ src/sdk/Pools.hpp
@@ -8,56 +8,56 @@ namespace rage
 	using Pool = IPool<T>;
 
 	class IPlayerPool
-		: public Pool<IPlayer>
+			: public Pool<IPlayer>
 	{
 	public:
 		// todo
 	};
 
 	class IVehiclePool
-		: public Pool<IVehicle>
+			: public Pool<IVehicle>
 	{
 	public:
 		// todo
 	};
 
 	class IColshapePool
-		: public Pool<IColshape>
+			: public Pool<IColshape>
 	{
 	public:
 		// todo
 	};
 
 	class ICheckpointPool
-		: public Pool<ICheckpoint>
+			: public Pool<ICheckpoint>
 	{
 	public:
 		// todo
 	};
 
 	class IMarkerPool
-		: public Pool<IMarker>
+			: public Pool<IMarker>
 	{
 	public:
 		// todo
 	};
 
 	class IBlipPool
-		: public Pool<IBlip>
+			: public Pool<IBlip>
 	{
 	public:
 		// todo
 	};
 
 	class IPickupPool
-		: public Pool<IPickup>
+			: public Pool<IPickup>
 	{
 	public:
 		// todo
 	};
 
 	class IObjectPool
-		: public Pool<IObject>
+			: public Pool<IObject>
 	{
 	public:
 		// todo
