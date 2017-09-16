bff8c34b57e5a00a32b752f655c8877749757753
Fabian Jungwirth <fabian@jungwirth.ovh>
2017-09-16 21:39:05 +0200

    Add licence headers

--- src/RageJavaCore.cpp
+++ src/RageJavaCore.cpp
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2017 Noxaro aka Fabian Jungwirth
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ * See the file COPYING included with this distribution for more information.
+ */
+
 #include "RageJavaCore.hpp"
 
 bool RageJavaCore::initialize(rage::IMultiplayer *mp) {
